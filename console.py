c7bbfb891a627e036e556dba3ae8915e537b62bc
GeebeeFrost <omgbolahan1@gmail.com>
2023-07-24 02:23:50 +0100

    Add test cases for new classes and console

--- console.py
+++ console.py
@@ -10,6 +10,10 @@ class HBNBCommand(cmd.Cmd):
     """Entry point of the command interpreter"""
     prompt = "(hbnb) "
 
+    def default(self, line):
+        """Catch non-matching commands."""
+        self.parse_cmd(line)
+
     def emptyline(self):
         """Do nothing when an empty line is entered"""
         pass
@@ -24,7 +28,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, line):
-        """Creates a new instance of BaseModel"""
+        """Creates a new instance of a specified class"""
         if not line:
             print("** class name missing **")
         elif not storage.actual_class(line):
@@ -52,7 +56,7 @@ class HBNBCommand(cmd.Cmd):
                     print(storage.all()[obj_id])
 
     def do_destroy(self, line):
-        """Deletes an instance"""
+        """Deletes an instance based on class name and id"""
         if not line:
             print("** class name missing **")
         else:
@@ -128,6 +132,51 @@ class HBNBCommand(cmd.Cmd):
                         pass
                 setattr(storage.all()[key], attr, val)
                 storage.all()[key].save()
+    
+    def do_count(self, line):
+        """Counts the instances of a class.
+        """
+        args = line.split(' ')
+        if not args[0]:
+            print("** class name missing **")
+        elif not storage.actual_class(args[0]):
+            print("** class doesn't exist **")
+        else:
+            matches = [
+                k for k in storage.all() if k.startswith(
+                    args[0] + '.')]
+            print(len(matches))
+
+    def parse_cmd(self, line):
+        """Intercepts commands to test for class.syntax()"""
+        match = re.search(r"^(\w*)\.(\w+)(?:\(([^)]*)\))$", line)
+        if not match:
+            return line
+        classname = match.group(1)
+        method = match.group(2)
+        args = match.group(3)
+        match_uid_and_args = re.search('^"([^"]*)"(?:, (.*))?$', args)
+        if match_uid_and_args:
+            uid = match_uid_and_args.group(1)
+            attr_or_dict = match_uid_and_args.group(2)
+        else:
+            uid = args
+            attr_or_dict = False
+
+        attr_and_value = ""
+        if method == "update" and attr_or_dict:
+            match_dict = re.search('^({.*})$', attr_or_dict)
+            if match_dict:
+                self.update_dict(classname, uid, match_dict.group(1))
+                return ""
+            match_attr_and_value = re.search(
+                '^(?:"([^"]*)")?(?:, (.*))?$', attr_or_dict)
+            if match_attr_and_value:
+                attr_and_value = (match_attr_and_value.group(
+                    1) or "") + " " + (match_attr_and_value.group(2) or "")
+        command = method + " " + classname + " " + uid + " " + attr_and_value
+        self.onecmd(command)
+        return command
 
 
 if __name__ == "__main__":
